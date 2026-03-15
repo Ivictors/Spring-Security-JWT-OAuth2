416a6fc84600ea87d787c54fb692baee323c6e28
Victor Marques <victor.marques.98v@gmail.com>
2026-03-14 23:01:56 -0300

    feat: Repository - foram cridas as interfaces para cada entidade

---
+++ src/main/java/com/victor/security2/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.victor.security2.repository;
+
+import com.victor.security2.entities.Role;
+import com.victor.security2.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/victor/security2/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.victor.security2.repository;
+
+import com.victor.security2.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
